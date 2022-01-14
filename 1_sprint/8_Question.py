677f4f569d1081104d37dfae76721cd8e62c9b6b
Misha86 <mpolishchuk86@gmail.com>
2022-01-14 13:47:00 +0200

    8 question

---
+++ 1_sprint/8_Question.py
@@ -0,0 +1,29 @@
+"""8 question 1 sprint"""
+"""a = [2, 2, 1, 3, 4, 1]"""
+
+def studying_hours(a: list) -> int:
+    """
+    Length of the maximum non-decreasing contiguous subarray
+    :param a: list
+    :return: int
+    """
+    j = 0
+    count_list = []
+    for i in a:
+        count = 0
+        max_i = 0
+        for q in a[j:]:
+            if q >= max_i:
+                max_i = q
+                count += 1
+            else:
+                break
+        j += 1
+        count_list.append(count)
+    return max(count_list)
+
+
+if __name__ == '__main__':
+    a = [2, 2, 10, 13, 18, 1, 3, 4, 1]
+    res = studying_hours(a)
+    print(res)
