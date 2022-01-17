5629700e06f84421e844865026c2718b038f63f5
Misha86 <mpolishchuk86@gmail.com>
2022-01-17 10:05:15 +0200

    2 sprint add 1_question

---
+++ 2_sprint/1_question.py
@@ -0,0 +1,11 @@
+"""1 question 2 sprint"""
+
+
+def double_string(d):
+    return  d
+
+
+if __name__ == '__main__':
+    data = ['aa', 'aaaa', 'abc', 'abcabc', 'qwer', 'qwerqwer']
+    result = double_string(data)
+    print(result)
