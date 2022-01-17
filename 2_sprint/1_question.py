e15af05973c158bf806698e0cda570de2ff9a4b4
Misha86 <mpolishchuk86@gmail.com>
2022-01-17 12:43:48 +0200

    2 sprint add 1_question done

--- 2_sprint/1_question.py
+++ 2_sprint/1_question.py
@@ -1,11 +1,22 @@
 """1 question 2 sprint"""
 
 
-def double_string(d):
-    return  d
+def double_string(d: list) -> int:
+    """Concatenate of two strings from this arguments  list
+    :param d: list
+    :return: int
+    """
+    total_count = 0
+    for s in d:
+        if s*2 in d:
+            total_count += 1
+        else:
+            for j in set(d):
+                total_count += True if (s + j) in d else False
+    return total_count
 
 
 if __name__ == '__main__':
-    data = ['aa', 'aaaa', 'abc', 'abcabc', 'qwer', 'qwerqwer']
+    data = ['aa', 'aaaa', 'aaaaaaaa', 'aaaa', 'qwer', 'qweraaaa']
     result = double_string(data)
     print(result)
