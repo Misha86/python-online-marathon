90b165090da8402785fe9bc6ca2bb60ce9ac40ab
Misha86 <mpolishchuk86@gmail.com>
2022-01-20 15:18:45 +0200

    3 sprint add 3_question done

---
+++ 3_sprint/3_3question.py
@@ -0,0 +1,48 @@
+"""3 question 3 sprint"""
+import re
+
+
+def create_account(user_name: str, password: str, secret_words: list):
+    match = re.match(r'^(?=.*[0-9].*)(?=.*[@#$*%^!&+=_].*)(?=.*[a-z].*)(?=.*[A-Z].*)[0-9a-zA-Z@#*$%^!&+=_]{6,}$',
+                     password)
+    if not match:
+        raise ValueError
+
+    def check(password_check: str, secret_words_check: list):
+        if password == password_check and len(secret_words) == len(secret_words_check):
+            m = 0
+            secret_words_copy = secret_words[:]
+            for c in secret_words_check:
+                if c in secret_words_copy:
+                    secret_words_copy.remove(c)
+                elif not m:
+                    m += 1
+                else:
+                    return False
+            return True
+        else:
+            return False
+    return check
+
+
+if __name__ == '__main__':
+    # val1 = create_account("123", "qQ1!45", ["1", "word"])
+    # # check1 = val1("Qwerty1_", ["1", "word"])
+
+    tom = create_account("Tom", "Qwerty1_", ["1", "word"])
+    check1 = tom("Qwerty1_", ["1", "word"])
+    check2 = tom("Qwerty1_", ["word"])
+    check3 = tom("Qwerty1_", ["word", "2"])
+    check4 = tom("Qwerty1!", ["word", "12"])
+    print(f"Check1: {check1}")
+    print(f"Check2: {check2}")
+    print(f"Check3: {check3}")
+    # print(f"Check4: {check4}")
+    print('-'*50)
+    tom2 = create_account("123", "qQ1!45", ['1', '2', '1'])
+    check1 = tom2("qQ1!45", ['2', '2', '1'] )
+    check2 = tom2("qQ1!45", ['1', 'word'])
+    check3 = tom2("qQ1!45", ["word", "3"])
+    print(f"Check1: {check1}")
+    print(f"Check2: {check2}")
+    print(f"Check3: {check3}")
---
+++ 3_sprint/3_4question.py
@@ -0,0 +1,8 @@
+"""4 question 3 sprint"""
+import re
+
+
+
+if __name__ == '__main__':
+    # val1 = create_account("123", "qQ1!45", ["1", "word"])
+    # # check1 = val1("Qwerty1_", ["1", "word"])
