1d19fad5e1b8c1be271f9024048d2cb0bf7deff9
Misha86 <mpolishchuk86@gmail.com>
2022-01-23 01:50:31 +0200

    4 sprint add 1_question done

--- 4_sprint/4_1question.py
+++ 4_sprint/4_1question.py
@@ -1,6 +1,27 @@
 """1 question 4 sprint"""
 
 
+class Employee:
+    """
+    Employee class, information about employees
+    """
+
+    def __init__(self, firstname, lastname, salary):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.salary = salary
+
+    @staticmethod
+    def from_string(s):
+        attr_list = s.split('-')
+        return Employee(attr_list[0], attr_list[1], int(attr_list[2]))
+
+
 if __name__ == '__main__':
-    olha = outer("Olha")
-    olha()
+    emp1 = Employee("Mary", "Sue", 60000)
+    emp2 = Employee.from_string("John-Smith-55000")
+    print(emp1.firstname)
+    print(emp1.salary)
+    print(emp2.firstname)
+
+
