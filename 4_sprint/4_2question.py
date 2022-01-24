d6db9767fb1e22023ad348a4cd84e0edba061938
Misha86 <mpolishchuk86@gmail.com>
2022-01-24 11:58:29 +0200

    4 sprint 2_question add method __set_order_number

--- 4_sprint/4_2question.py
+++ 4_sprint/4_2question.py
@@ -5,15 +5,21 @@ class Pizza:
     """
     Order Pizza
     """
-    __order_number = 0
+    __order_numbers = {}
+    __count = 0
 
     def __init__(self, ingredients: list):
         self.ingredients = ingredients
+        self.__set_order_number()
+        self.__order_numbers[self] = self.__count
+
+    @classmethod
+    def __set_order_number(cls):
+        cls.__count += 1
 
     @property
     def order_number(self):
-        self.__class__.__order_number += 1
-        return self.__class__.__order_number
+        return self.__order_numbers[self]
 
     @classmethod
     def garden_feast(cls):
@@ -34,5 +40,6 @@ if __name__ == '__main__':
     print(p1.ingredients)
     print(p2.ingredients)
     print(p1.order_number)
+    print(p2.order_number)
     print(p2.order_number)
 
