1930a60b7f3f24575b3fa96fb9406f3521e1bff5
Misha86 <mpolishchuk86@gmail.com>
2022-01-27 17:08:52 +0200

    5 sprint 7_question changed if

--- 5_sprint/5_7question.py
+++ 5_sprint/5_7question.py
@@ -1,20 +1,22 @@
-"""6 question 5 sprint"""
-import calendar
+"""7 question 5 sprint"""
+import cmath
 
 
-def day_of_week(day):
+def solve_quadric_equation(a, b, c):
     try:
-        int_day = int(day)
-        if int_day in range(1, 8):
-            return calendar.day_name[int_day-1]
-        else:
-            return "There is no such day of the week! Please try again."
+        D = float(b)**2 - 4*float(a)*float(c)
+        if D != 0:
+            x1 = (-b - cmath.sqrt(D))/(2 * a)
+            x2 = (-b + cmath.sqrt(D))/(2*a)
+            return f"The solution are {x1=} and {x2=}"
+    except ZeroDivisionError:
+        return "Zero Division Error"
     except ValueError:
-        return "You did not enter a number! Please try again."
+        return "Could not convert string to float"
 
 
 if __name__ == '__main__':
-    print(day_of_week(2))  # output:   "Tuesday"
-    print(day_of_week(11))  # output:  "There is no such day of the week! Please try again."
-    print(day_of_week("Monday"))  # output:   "You did not enter a number! Please try again."
-
+    print(solve_quadric_equation(1, 5, 6))  # output:   " The solution are x1=(-2-0j) and x2=(-3+0j)"
+    print(solve_quadric_equation(0, 8, 1))  # output:   "Zero Division Error"
+    print(solve_quadric_equation(1, "abc", 5))  # output:   "Could not convert string to float"
+    print(solve_quadric_equation(1, 3, -4))
