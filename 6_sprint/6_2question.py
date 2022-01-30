4045f61e3091cbecd5cd6e6cd7b557720ddf6819
Misha86 <mpolishchuk86@gmail.com>
2022-01-30 19:44:27 +0200

    6 sprint 2_question changed

--- 6_sprint/6_2question.py
+++ 6_sprint/6_2question.py
@@ -14,33 +14,12 @@ def parse_user(output_file, *input_files):
                 with open(file, 'r') as f:
                     def get_name(dct):
                         if "name" in dct and dct["name"] not in users_name_list:
-                            val = dct.get('name')
-                            users_name_list.append(val)
+                            users_name_list.append(dct["name"])
                             users_list.append(dct)
-                            return dct
                     json.load(f, object_hook=get_name)
             except FileNotFoundError:
-                logging.error(f"File {file} doesn't exists")
+                logging.error(f"File {file} doesn't exist")
         json.dump(users_list, res_file, indent=4)
-
-
-def parse_user2(output_file, *input_files):
-    with open(output_file, "w") as res_file:
-        users_list = []
-        for file in input_files:
-            try:
-                with open(file, 'r') as f:
-                    users_list.extend(json.load(f))
-            except FileNotFoundError:
-                logging.error(f"File {file} doesn't exists")
-        name_list = list(set(map(lambda x: x['name'], users_list)))
-
-        def filter_name(dct):
-            if dct["name"] in name_list:
-                name_list.remove(dct["name"])
-                return True
-        users_list_f = filter(filter_name, users_list)
-        json.dump(list(users_list_f), res_file, indent=4)
 
 
 if __name__ == '__main__':
