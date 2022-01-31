16c8169c78258bbbb4bf13e07aae59920cab96df
Misha86 <mpolishchuk86@gmail.com>
2022-01-31 16:48:30 +0200

    6 sprint 1_question added second implementation

--- 6_sprint/6_1question.py
+++ 6_sprint/6_1question.py
@@ -1,18 +1,34 @@
 """1 question 6 sprint"""
 import json
+from json import JSONDecoder
+
+
+class PasswordDecoderFind2(JSONDecoder):
+    def __init__(self, key, values_set):
+        super().__init__(object_hook=self.dict_to_object)
+        self.key = key
+        self.got_values = values_set
+
+    def dict_to_object(self, dictionary):
+        if self.key in dictionary:
+            value = dictionary[self.key]
+            self.got_values.update(value) if isinstance(value, list) else self.got_values.add(value)
+
+
+def find2(file, key):
+    with open(file, 'r') as f:
+        passwords_set = set()
+        json.load(f, object_hook=PasswordDecoderFind2(key, passwords_set).dict_to_object)
+        return list(passwords_set)
 
 
 def find(file, key):
+    passwords_set = set()
     with open(file, 'r') as f:
-        passwords_set = set()
-
         def get_password(dct):
             if key in dct:
-                d = dct.get(key)
-                if isinstance(d, list):
-                    passwords_set.update(d)
-                else:
-                    passwords_set.add(d)
+                value = dct.get(key)
+                passwords_set.update(value) if isinstance(value, list) else passwords_set.add(value)
         json.load(f, object_hook=get_password)
         return list(passwords_set)
 
