0417e5526ee76cf85407031ec99eef9b1d1ea245
Misha86 <mpolishchuk86@gmail.com>
2022-02-02 10:50:38 +0200

    6 sprint 4_question changed __str___ cGroup

--- 6_sprint/6_4question.py
+++ 6_sprint/6_4question.py
@@ -23,7 +23,7 @@ class Group:
         self.students = students
 
     def __str__(self):
-        return f"{self.title}: {[s.__str__() for s in self.students]}"
+        return f"{self.title}: {list(map(str, self.students))}"
 
     @staticmethod
     def serialize_to_json(list_of_groups, filename):
@@ -33,12 +33,19 @@ class Group:
     @classmethod
     def create_group_from_file(cls, students_file):
         with open(students_file) as f:
-            load_data = json.load(f)
-            return cls(students_file.rstrip('.json'),
-                       [Student(**i) for i in load_data] if isinstance(load_data, list) else [Student(**load_data)])
+            students = json.load(f, object_hook=lambda dct: Student(**dct))
+            return cls(students_file.rstrip('.json'), students if isinstance(students, list) else [students])
 
 
 if __name__ == '__main__':
-    g = Group.create_group_from_file("students.json")
-    print(g.students)
-    print(g.title)
+    g1 = Group.create_group_from_file("2020_2.json")
+    print(g1)
+    g2 = Group.create_group_from_file("2020-01.json")
+    print(g2)
+    Group.serialize_to_json([g1, g2], "g1.json")
+    f1 = open("g1.json")
+    d = json.load(f1)
+    s = [{"title": "2020_2", "students": [{"full_name": "Student 1 from second Group", "avg_rank": 98, "courses": ["Python"]}, {"full_name": "Student 2 from second Group", "avg_rank": 70.34, "courses": ["Ruby", "Python", "Frontend development"]}]}, {"title": "2020-01", "students": [{"full_name": "Student2 from group2", "avg_rank": 50.4, "courses": ["C++"]}]}]
+    print(d == s)
+    print(d)
+    f1.close()
