a6b244252587a0771b706928010df6ee14eeb001
Misha86 <mpolishchuk86@gmail.com>
2022-02-04 16:47:46 +0200

    7 sprint 5_question 'Composite Method' has done

--- 6_sprint/6_3question.py
+++ 6_sprint/6_3question.py
@@ -1,5 +1,7 @@
 """3 question 6 sprint"""
 import json
+
+import jsonschema
 from jsonschema import validate
 import csv
 
@@ -41,51 +43,33 @@ def validate_json(data, schema):
     try:
         validate(data, schema)
         return data
-    except:
+    except jsonschema.exceptions.ValidationError:
         raise InvalidInstanceError(f"Error in {schema['owner']} schema")
 
 
 def user_with_department(csv_file, user_json, department_json):
-    with open(csv_file, 'w') as f, open(user_json) as user_file, open(department_json) as department_file:
+    def get_user_department(dct):
+        validate_json(dct, user_schema)
+        dct.pop('id')
+        department_id = dct.pop('department_id')
+        department_name = (dep_item.get("name") for dep_item in department_json_load
+                           if dep_item.get('id') == department_id)
+
+        if not department_name:
+            raise DepartmentName(f"Department with id {department_id} doesn't exists")
+
+        dct['department'] = next(department_name, None)
+        return dct
+
+    with open(user_json) as user_file, open(department_json) as department_file:
+        department_json_load = validate_json(json.load(department_file), department_schema)
+        user_json_load = json.load(user_file, object_hook=get_user_department)
+
+    with open(csv_file, 'w') as f:
         writer = csv.DictWriter(f, fieldnames=["name", "department"])
         writer.writeheader()
-        try:
-            department_json_load = validate_json(json.load(department_file), department_schema)
-
-            def get_user_department(dct):
-                validate_json(dct, user_schema)
-                dct.pop('id')
-                department_id = dct.pop('department_id')
-                department_name = [dep_item.get("name") for dep_item in department_json_load
-                                   if dep_item.get('id') == department_id]
-
-                if not department_name:
-                    raise DepartmentName(f"Department with id {department_id} doesn't exists")
-
-                dct['department'] = department_name[0]
-                return dct
-
-            user_json_load = json.load(user_file, object_hook=get_user_department)
-            writer.writerows(user_json_load)
-
-        except InvalidInstanceError as e:
-            print(str(e))
-
-        except DepartmentName as e:
-            print(str(e))
+        writer.writerows(user_json_load)
 
 
 if __name__ == '__main__':
-    # user_instance = {
-    #     "id": 1,
-    #     "name": "userName",
-    #     "department_id": 1
-    # }
-    #
-    # department_instance = {
-    #     "id": 1,
-    #     "name": "departmentName",
-    # }
-    # print(validate_json(user_instance, user_schema))
-    # print(validate_json(department_instance, department_schema))
-    print(user_with_department('csv_file.csv', 'user_json.json', 'department_json.json'))
+    user_with_department('csv_file.csv', 'user_json.json', 'department_json.json')
---
+++ 7_sprint/7_5question_composite_method.py
@@ -0,0 +1,83 @@
+"""1 question 7 sprint"""
+
+
+class LeafElement:
+
+    def __init__(self, *args):
+        """Takes the first positional argument and assigns to member variable "position"."""
+        self.position = args[0]
+
+    def showDetails(self):
+        """Prints the position of the child element."""
+        print(self.position)
+
+
+class CompositeElement:
+
+    def __init__(self, *args):
+        """Takes the first positional argument and assigns to member
+         variable "position". Initializes a list of children elements."""
+        self.position = args[0]
+        self._children = []
+
+    def add(self, child):
+        """Adds the supplied child element to the list of children
+         elements "children"."""
+        self._children.append(child)
+
+    def remove(self, child):
+        """Removes the supplied child element from the list of
+        children elements "children"."""
+        self._children.remove(child)
+
+    def showDetails(self):
+        """Prints the details of the component element first. Then,
+        iterates over each of its children, prints their details by
+        calling their showDetails() method."""
+        print(self.position)
+        for child in self._children:
+            if isinstance(child, CompositeElement):
+                print("\t", child.position, sep="")
+                for c in child._children:
+                    print("\t\t", end="")
+                    c.showDetails()
+            else:
+                print("\t", end="")
+                child.showDetails()
+
+
+if __name__ == '__main__':
+    manager1 = CompositeElement("Manager1")
+    developer11 = LeafElement("Developer11")
+    developer12 = LeafElement("Developer12")
+    manager1.add(developer11)
+    manager1.add(developer12)
+    manager1.showDetails()
+
+    manager2 = CompositeElement("Manager2")
+    developer21 = LeafElement("Developer21")
+    developer22 = LeafElement("Developer22")
+    manager2.add(developer21)
+    manager2.add(developer22)
+    manager1.showDetails()
+
+    general_manager = CompositeElement("GeneralManager")
+    general_manager.add(manager1)
+    general_manager.add(manager2)
+    general_manager.showDetails()
+
+    topLevelMenu = CompositeElement("GeneralManager")
+    subMenuItem1 = CompositeElement("Manager1")
+    subMenuItem2 = CompositeElement("Manager2")
+    subMenuItem11 = LeafElement("Developer11")
+    subMenuItem12 = LeafElement("Developer12")
+    subMenuItem21 = LeafElement("Developer21")
+    subMenuItem22 = LeafElement("Developer22")
+    subMenuItem1.add(subMenuItem11)
+    subMenuItem1.add(subMenuItem12)
+    subMenuItem2.add(subMenuItem22)
+    subMenuItem2.add(subMenuItem22)
+    subMenuItem2.add(subMenuItem22)
+    topLevelMenu.add(subMenuItem1)
+    topLevelMenu.add(subMenuItem2)
+    topLevelMenu.showDetails()
