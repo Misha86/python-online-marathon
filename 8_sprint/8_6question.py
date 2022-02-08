b6cbb668998f3176c7e96160e20c03df407af759
Misha86 <mpolishchuk86@gmail.com>
2022-02-08 11:31:44 +0200

    8 sprint 6_question

--- 8_sprint/8_6question.py
+++ 8_sprint/8_6question.py
@@ -1,10 +1,10 @@
 """6 question 8 sprint"""
 import unittest
-from unittest.mock import Mock
+from unittest.mock import patch, mock_open
 
 
 def file_parser(file_path, find_str: str, replace_str: str = None) -> str:
-    with open(file_path, 'r') as f:
+    with open(file_path, 'r+') as f:
         data = f.read()
         count = data.count(find_str)
         if replace_str:
@@ -17,11 +17,39 @@ def file_parser(file_path, find_str: str, replace_str: str = None) -> str:
 
 
 class ParserTest(unittest.TestCase):
-    pass
+
+    str_f = "These are short, famous texts in English from classic sources like the Bible " \
+            "or Shakespeare. Some texts have word definitions and explanations to help you."
+
+    def setUp(self):
+        self.file_parser = file_parser
+        self.search_string = "texts"
+
+    def test_count_string(self):
+        with patch('__main__.open', mock_open(read_data=self.str_f)) as file:
+            result = self.file_parser(file, self.search_string)
+            self.assertEqual(result, "Found 2 strings")
+            file.assert_called_once()
+
+    def test_replace_string(self):
+        with patch('__main__.open', mock_open(read_data=self.str_f)) as file:
+            result = self.file_parser(file, self.search_string, "No data")
+            self.assertEqual(result, "Replaced 2 strings")
+            file.assert_called_once()
+
+    @unittest.expectedFailure
+    def test_file_exist(self):
+        self.file_parser("not_exist_file.txt", "string")
+
+    @patch("__main__.file_parser", return_value=f"Found 3 strings")
+    def test_file_parser(self, parser_mock):
+        self.file_parser = parser_mock
+        self.assertEqual(self.file_parser("file.txt", "string"), "Found 3 strings")
+        self.file_parser.assert_called_once_with("file.txt", "string")
 
 
 if __name__ == '__main__':
     print(file_parser("grades.json", "subject_id"))
-    # unittest.main()
+    unittest.main(verbosity=2)
 
 
