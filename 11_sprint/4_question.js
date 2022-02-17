124f0c76cbfb046f0e05a5d43f3792e96daa6e10
Misha86 <mpolishchuk86@gmail.com>
2022-02-17 23:16:11 +0200

    11_sprint 4_question has done

---
+++ 11_sprint/4_question.js
@@ -0,0 +1,10 @@
+/* 4 question 11 sprint */
+
+const sumOfLen = (...words) => {
+    let result = words ? words.reduce((total, word) => total + word.length, 0): 0;
+    return result
+}
+
+	
+console.log(sumOfLen('hello', 'hi'));
+console.log(sumOfLen());
